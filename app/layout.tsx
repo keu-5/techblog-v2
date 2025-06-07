3380062ff2114f4511ff7e4d61f926bbd699a569
keu-5 <keu02.78@gmail.com>
2025-06-07 20:43:12 +0900

    fix: 404にサイドバーを反映指せないようにした

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Source_Code_Pro } from "next/font/google";
 
-import { LeftSidebar } from "@/app/left-sidebar";
 import { ThemeToggle } from "@/components/composite/theme-toggle";
 import { Toaster } from "@/components/ui/sonner";
-import { findDocs } from "@/features/searchResults/repositories/searchResultRepository";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const sourceCodePro = Source_Code_Pro({
@@ -24,8 +22,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { docs } = findDocs();
-
   return (
     <html lang="ja">
       <body
@@ -44,32 +40,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div
-            className={`
-                h-full
-                md:mx-12 md:mt-12
-              `}
-          >
-            <div
-              className={`
-                  left-12 top-12 z-[105] w-full p-4
-                  md:fixed md:h-screen md:w-1/4
-                  lg:w-1/5
-                `}
-            >
-              <LeftSidebar docs={docs} />
-            </div>
-
-            <div
-              className={`
-                  size-full p-4
-                  md:ml-[25%] md:w-3/4
-                  lg:ml-[20%] lg:w-3/5
-                `}
-            >
-              {children}
-            </div>
-          </div>
+          {children}
 
           <Toaster />
           <ThemeToggle />
